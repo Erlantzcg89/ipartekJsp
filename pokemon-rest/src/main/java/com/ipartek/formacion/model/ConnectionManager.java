a4e9d7594ce41d6d90d7101df42e560a906f9325
Erlantzcg89 <erlantzcg89@gmail.com>
2020-01-29 14:00:02 +0100

    plantilla para servicio rest

---
+++ pokemon-rest/src/main/java/com/ipartek/formacion/model/ConnectionManager.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.model;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+
+
+public class ConnectionManager {
+
+	private final static Logger LOG = Logger.getLogger(ConnectionManager.class);
+	private static Connection conn;
+
+	public static Connection getConnection() {
+
+		conn = null;
+
+		try {
+			InitialContext ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mydb");
+
+			if (ds == null) {
+				throw new Exception("Data source no encontrado!");
+			}
+
+			conn = ds.getConnection();
+
+		} catch (Exception e) {
+
+			LOG.fatal(e);
+		}
+
+		return conn;
+
+	}
+	
+}
---
+++ pokemon-rest/src/main/java/com/ipartek/formacion/model/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Capa del Modelo, que facilita el acceso a datos almacenados.
+ */
+package com.ipartek.formacion.model;
