0f79005ba0b4f91b391d4f9fb261c65c587c8258
Erlantzcg89 <erlantzcg89@gmail.com>
2020-02-03 13:48:30 +0100

    base acabada

--- mf0966-3-erlantz/src/main/java/com/ipartek/formacion/mf0966/modelo/CursoDAO.java
+++ mf0966-3-erlantz/src/main/java/com/ipartek/formacion/mf0966/modelo/CursoDAO.java
@@ -36,9 +36,9 @@ public class CursoDAO implements IDAO<Curso>{
 
 		LOG.debug("Entra en getAll");
 
-		String sql = "SELECT c.id 'id_curso', c.nombre 'nombre_curso', c.finicio 'inicio', c.ffin 'fin', c.horas 'horas', c.id_profesor 'profesor', v.id 'id_valoracion', v.comentario 'comentario', v.nota 'nota', v.id_usuario 'id_usuario', v.id_curso 'curso' FROM curso c, valoraciones v where v.id_curso=c.id ORDER BY c.id asc LIMIT 500;";
+		ArrayList<Curso> resultado = new ArrayList<Curso>();
 
-		HashMap<Integer, Curso> mapCursos = new HashMap<Integer, Curso>();
+		String sql = "SELECT c.id 'id_curso', c.nombre 'nombre_curso', c.finicio 'inicio', c.ffin 'fin', c.horas 'horas', c.id_profesor 'profesor' FROM curso c ORDER BY c.id asc LIMIT 500;";
 
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pst = con.prepareStatement(sql);
@@ -48,25 +48,13 @@ public class CursoDAO implements IDAO<Curso>{
 
 			while (rs.next()) {
 
-				if (!mapCursos.containsKey(rs.getInt("id_curso"))) {
-
-					mapCursos.put(rs.getInt("id_curso"), mapper(rs));
-
-				} else {
-
-					mapCursos.get(rs.getInt("id_curso")).getValoraciones().add(mapperValoracion(rs));
-
-				}
+				resultado.add(mapper(rs));
 
 			}
 
 		} catch (Exception e) {
 			LOG.error(e);
 		}
-
-		Collection<Curso> coleccionCursos = mapCursos.values();
-
-		ArrayList<Curso> resultado = new ArrayList<Curso>(coleccionCursos);
 
 		return resultado;
 	}
@@ -138,24 +126,6 @@ public class CursoDAO implements IDAO<Curso>{
 
 		return c;
 	}
-	
-	/**
-	 * Utilidad para mapear una Valoracion
-	 *
-	 * @param rs ResulSet
-	 * @return Habilidad
-	 * @throws SQLException
-	 */
-	private Valoracion mapperValoracion(ResultSet rs) throws SQLException {
 
-		Valoracion v = new Valoracion();
-		v.setId(rs.getInt("id_valoracion"));
-		v.setComentario(rs.getString("comentario"));
-		v.setNota(rs.getInt("nota"));
-		v.setIdUsuario(rs.getInt("id_usuario"));
-		v.setIdCurso(rs.getInt("curso"));
-
-		return v;
-	}
 	
 }
